2c8cc740dfe814b6e66d00f4352059b8876e2b94
ramesh khanal <ramesh.khanal@swifttech.com>
2020-11-09 14:03:21 +0545

    menu fixed

--- js/main.js
+++ js/main.js
@@ -23,5 +23,32 @@
 			jQuery('html, body').animate({scrollTop : 0},800);
 			return false;
 		});
+
+		$("#mobile-menu-toggle").on("click", function(e) {
+            var element = document.querySelector( '.main-navigation' );
+            var focusable = element.querySelectorAll( 'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+            var firstFocusable = focusable[0];
+            var lastFocusable = focusable[focusable.length - 1];
+            tab_focus( firstFocusable, lastFocusable );
+        });
+        //Focus trap in popup.
+     
+        function tab_focus( firstFocusable, lastFocusable ) {
+            $(document).on('keydown', function(e) {
+                if (e.key === 'Tab' || e.keyCode === KEYCODE_TAB) {
+                    if ( e.shiftKey ) /* shift + tab */ {
+                        if (document.activeElement === firstFocusable) {
+                            lastFocusable.focus();
+                            e.preventDefault();
+                        }
+                    } else /* tab */ {
+                        if (document.activeElement === lastFocusable) {
+                            firstFocusable.focus();
+                            e.preventDefault();
+                        }
+                    }
+                }
+            });
+        }
 	}
 )(jQuery);
